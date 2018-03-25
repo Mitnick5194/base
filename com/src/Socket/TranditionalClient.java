f2c0845db66e385bbf9ef1b8fa82888f97596d38
Mitnick5194 <qq519441630@gmail.com>
2018-03-25 21:06:43 +0800

    Update TranditionalClient.java

--- com/src/Socket/TranditionalClient.java
+++ com/src/Socket/TranditionalClient.java
@@ -26,8 +26,10 @@ public class TranditionalClient {
 
 	public void connect() {
 		try {
+			//会阻塞到连接成功 连接成功不是服务端的accept 这根本就不管服务端的线程问题 只是找到服务端这个地址和端口而已 所以服务端此时
+			//设置了线程睡眠 这里照样能连接 而如果因为网络慢原因 很久没有找到服务端的地址和端口 就会阻塞到找到为止 或者抛异常
 			socket.connect(new InetSocketAddress("127.0.0.1", 9999));
-			// 服务端睡眠了 也能到这一步 所以说上面的connect不是阻塞的
+			// 服务端睡眠了 也能到这一步 所以说上面的connect不是阻塞的(其实是错误的 看上面解释)
 			// 但是服务端不会处理请求 下面的逻辑就是阻塞的了
 			System.out.println("客户端连接成功");
 			outputStream = socket.getOutputStream();
@@ -47,7 +49,7 @@ public class TranditionalClient {
 	public static void main(String[] args) {
 		AtomicInteger count = new AtomicInteger();
 		// 只用主线程创建1000个连接线程 这里执行到一定时期会报错 不能连接上 因为连接数有限
-		for (int i = 0; i < 50; i++) {
+		for (int i = 0; i < 5; i++) {
 			new Thread(new Runnable() {
 				@Override
 				public void run() {
@@ -55,7 +57,6 @@ public class TranditionalClient {
 					client.connect();
 				}
 			}, count.incrementAndGet() + "").start();
-			;
 		}
 	}
 
